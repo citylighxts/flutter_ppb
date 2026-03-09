a880bddcd6fe5a71f935d99b667e29faedcbfff7
citylighxts <hanazizahn@gmail.com>
2026-03-09 10:38:02 +0700

    feat: change to "final code reference" code

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,55 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('my first app'),
-      centerTitle: true,
-      backgroundColor: Colors.red[600],
-    ),
-    body: Center(
-      child: Text(
-        'hello ninjas',
-        style: TextStyle(
-          fontSize: 30,
-          fontWeight: FontWeight.bold,
-          letterSpacing: 2.0,
-          color: Colors.blueAccent,
-          fontFamily: 'IndieFlower', 
+// void main() => runApp(MaterialApp(
+//   home: Scaffold(
+//     appBar: AppBar(
+//       title: Text('my first app'),
+//       centerTitle: true,
+//       backgroundColor: Colors.red[600],
+//     ),
+//     body: Center(
+//       child: Text(
+//         'hello ninjas',
+//         style: TextStyle(
+//           fontSize: 30,
+//           fontWeight: FontWeight.bold,
+//           letterSpacing: 2.0,
+//           color: Colors.blueAccent,
+//           fontFamily: 'IndieFlower', 
+//         ),
+//       ),
+//     ),
+//     floatingActionButton: FloatingActionButton(
+//       backgroundColor: Colors.amber,
+//       child: Text('click'),
+//       onPressed: () {},
+//     ),
+//   ),
+// ));
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.amber,
+        title: const Text('...'),
+      ),
+      body: Center(
+        child: Text(
+          'Henshin',
+          style: TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 3,
+            color: Colors.blueAccent,
+          ),
         ),
       ),
-    ),
-    floatingActionButton: FloatingActionButton(
-      backgroundColor: Colors.amber,
-      child: Text('click'),
-      onPressed: () {},
-    ),
-  ),
-));
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.amber,
+        child: const Icon(Icons.add),
+        onPressed: () {},
+      ),
+    )
+  ));
+}
