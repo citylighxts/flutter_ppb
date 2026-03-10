a2f41fe17a6d60a0fadcd16c87c8213127a7af66
citylighxts <hanazizahn@gmail.com>
2026-03-10 11:33:41 +0700

    feat: add widgets and readme explanation

--- lib/main.dart
+++ lib/main.dart
@@ -1,55 +1,131 @@
 import 'package:flutter/material.dart';
 
-// void main() => runApp(MaterialApp(
-//   home: Scaffold(
-//     appBar: AppBar(
-//       title: Text('my first app'),
-//       centerTitle: true,
-//       backgroundColor: Colors.red[600],
-//     ),
-//     body: Center(
-//       child: Text(
-//         'hello ninjas',
-//         style: TextStyle(
-//           fontSize: 30,
-//           fontWeight: FontWeight.bold,
-//           letterSpacing: 2.0,
-//           color: Colors.blueAccent,
-//           fontFamily: 'IndieFlower', 
-//         ),
-//       ),
-//     ),
-//     floatingActionButton: FloatingActionButton(
-//       backgroundColor: Colors.amber,
-//       child: Text('click'),
-//       onPressed: () {},
-//     ),
-//   ),
-// ));
-
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const RowColumnPage(),
+    );
+  }
+}
+
+class RowColumnPage extends StatelessWidget {
+  const RowColumnPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    MediaQueryData mediaQueryData = MediaQuery.of(context);
+    double screenWidth = mediaQueryData.size.width;
+    double screenHeight = mediaQueryData.size.height;
+    return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.amber,
-        title: const Text('...'),
-      ),
-      body: Center(
-        child: Text(
-          'Henshin',
-          style: TextStyle(
-            fontSize: 30,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 3,
-            color: Colors.blueAccent,
-          ),
+        title: const Text(
+          'My First App',
+          style: TextStyle(color: Colors.black),
         ),
+        backgroundColor: Colors.orange[200],
+        centerTitle: true,
       ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.amber,
-        child: const Icon(Icons.add),
-        onPressed: () {},
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            child: AspectRatio(
+              aspectRatio: 1.0,
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                margin: EdgeInsets.fromLTRB(20.0, 5.0, 20.0, 10.0),
+                padding: EdgeInsets.all(20.0),
+                color: Colors.lightBlue[100],
+                child: Center(
+                  child: Image.network(
+                    'https://picsum.photos/200',
+                    fit: BoxFit.cover,
+                    width: 500,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            margin: EdgeInsets.fromLTRB(20.0, 5.0, 20.0, 10.0),
+            padding: EdgeInsets.all(20.0),
+            color: Colors.pink[200],
+            child: Text('What image is that', style: TextStyle(fontSize: 16)),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            color: Colors.yellow[200],
+            padding: EdgeInsets.all(20.0),
+            margin: EdgeInsets.fromLTRB(20.0, 5.0, 20.0, 5.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Column(children: [Icon(Icons.food_bank), Text("Food")]),
+                Column(children: [Icon(Icons.landscape), Text("Scenery")]),
+                Column(children: [Icon(Icons.people), Text("People")]),
+              ],
+            ),
+          ),
+          CounterCard(),
+        ],
       ),
-    )
-  ));
+    );
+  }
+}
+
+class CounterCard extends StatefulWidget {
+  const CounterCard({super.key});
+
+  @override
+  State<CounterCard> createState() => _CounterCardState();
+}
+
+class _CounterCardState extends State<CounterCard> {
+  int _counter = 0; // This is the state (data) that changes.
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++; // Update the state.
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.fromLTRB(20.0, 5.0, 20.0, 5.0),
+      padding: EdgeInsets.all(20.0),
+      width: MediaQuery.of(context).size.width,
+      color: Colors.cyan[100],
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text("Counter here: $_counter", style: TextStyle(fontSize: 16)),
+          Container(
+            color: Colors.cyan[200],
+            padding: EdgeInsets.all(5.0),
+            child: IconButton(
+              onPressed: _incrementCounter,
+              icon: Icon(Icons.add, color: Colors.black, size: 16),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
